18b5ae665a8ee3dfa91e117aad29afd4ed24c89a
Christopher Dorst <chris@dorst.email>
2017-12-12 15:36:03 -0600

    Adding AttributeLists helper

---
+++ DevOps.Primitives.CSharp.Helpers.Common/AttributeLists.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class AttributeLists
+    {
+        public static AttributeListCollection Create(params Attribute[] attributes)
+            => new AttributeListCollection(GetListItems(attributes));
+
+        private static List<AttributeListCollectionAssociation> GetListItems(params Attribute[] attributes)
+            => attributes.Select(attribute => new AttributeListCollectionAssociation(attribute)).ToList();
+    }
+}
