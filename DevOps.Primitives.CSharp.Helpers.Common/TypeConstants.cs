b1ad790277687cd86176c4a1c0fae7cb6bc0fe83
Christopher Dorst <chris@dorst.email>
2017-12-12 15:55:20 -0600

    Adding date, double, and nullable TypeConstants

--- DevOps.Primitives.CSharp.Helpers.Common/TypeConstants.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/TypeConstants.cs
@@ -3,10 +3,21 @@
     public static class TypeConstants
     {
         public const string Byte = "byte";
+        public const string ByteNullable = "byte?";
+        public const string DateTime = "DateTime";
+        public const string DateTimeNullable = "DateTime?";
+        public const string DateTimeOffset = "DateTimeOffset";
+        public const string DateTimeOffsetNullable = "DateTimeOffset?";
         public const string Decimal = "decimal";
+        public const string DecimalNullable = "decimal?";
+        public const string Double = "double";
+        public const string DoubleNullable = "double?";
         public const string Int = "int";
+        public const string IntNullable = "int?";
         public const string Long = "long";
+        public const string LongNullable = "long?";
         public const string Short = "short";
+        public const string ShortNullable = "short?";
         public const string String = "string";
     }
 }
