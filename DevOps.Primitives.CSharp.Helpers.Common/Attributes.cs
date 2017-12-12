25e22148d94520ba8d4901faedab131cee992f7e
Christopher Dorst <chris@dorst.email>
2017-12-12 14:02:11 -0600

    Adding protobuf attribute helpers

--- DevOps.Primitives.CSharp.Helpers.Common/Attributes.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/Attributes.cs
@@ -3,6 +3,8 @@
     public static class Attributes
     {
         public static Attribute Key() => new Attribute(nameof(Key));
+        public static Attribute ProtoContract() => new Attribute(nameof(ProtoContract));
+        public static Attribute ProtoMember(int tag) => new Attribute(nameof(ProtoMember), tag.ToString());
         public static Attribute Required() => new Attribute(nameof(Required));
     }
 }
