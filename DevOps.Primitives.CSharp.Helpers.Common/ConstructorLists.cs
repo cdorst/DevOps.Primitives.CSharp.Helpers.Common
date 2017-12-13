9a6d834e20607d2393327946b23176bf0fc5f161
Christopher Dorst <chris@dorst.email>
2017-12-12 21:53:06 -0600

    Adding ConstructorLists helper

---
+++ DevOps.Primitives.CSharp.Helpers.Common/ConstructorLists.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class ConstructorLists
+    {
+        public static ConstructorList Create(params Constructor[] attributes)
+            => new ConstructorList(GetListItems(attributes));
+
+        private static List<ConstructorListAssociation> GetListItems(params Constructor[] attributes)
+            => attributes.Select(attribute => new ConstructorListAssociation(attribute)).ToList();
+    }
+}
