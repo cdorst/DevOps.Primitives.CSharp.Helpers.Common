4f7a82c6fff598fc2646ccbb8ad360d253443b9b
Christopher Dorst <chris@dorst.email>
2017-12-12 21:51:04 -0600

    Adding default constructor helper

---
+++ DevOps.Primitives.CSharp.Helpers.Common/Constructors.cs
@@ -0,0 +1,8 @@
+﻿namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class Constructors
+    {
+        public static Constructor Default(string name)
+            => new Constructor(name, new Block(), ModifierLists.Public);
+    }
+}
