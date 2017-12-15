47a32787a84c6a5f2f7207be4c4f0f0d7461c885
Christopher Dorst <chris@dorst.email>
2017-12-14 21:54:22 -0600

    Adding string[] overload to BaseLists.Create

--- DevOps.Primitives.CSharp.Helpers.Common/BaseLists.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/BaseLists.cs
@@ -8,6 +8,9 @@ namespace DevOps.Primitives.CSharp.Helpers.Common
         public static BaseList Create(params BaseType[] baseTypes)
             => new BaseList(GetListItems(baseTypes));
 
+        public static BaseList Create(params string[] baseTypes)
+            => Create(baseTypes.Select(b => new BaseType(b)).ToArray());
+
         private static List<BaseListAssociation> GetListItems(params BaseType[] baseTypes)
             => baseTypes.Select(baseType => new BaseListAssociation(baseType)).ToList();
     }
