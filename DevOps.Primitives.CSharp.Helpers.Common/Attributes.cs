60d14165533171c16415720c0626894dd9678743
Christopher Dorst <chris@dorst.email>
2017-12-14 23:51:54 -0600

    Adding Attributes.Table helper

--- DevOps.Primitives.CSharp.Helpers.Common/Attributes.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/Attributes.cs
@@ -6,5 +6,11 @@
         public static Attribute ProtoContract() => new Attribute(nameof(ProtoContract));
         public static Attribute ProtoMember(int tag) => new Attribute(nameof(ProtoMember), tag.ToString());
         public static Attribute Required() => new Attribute(nameof(Required));
+        public static Attribute Table(string name, string schema = null)
+        {
+            var args = $"\"{name}\"";
+            if (!string.IsNullOrWhiteSpace(schema)) args = $"{args}, Schema = \"{schema}\"";
+            return new Attribute(nameof(Table), args);
+        }
     }
 }
