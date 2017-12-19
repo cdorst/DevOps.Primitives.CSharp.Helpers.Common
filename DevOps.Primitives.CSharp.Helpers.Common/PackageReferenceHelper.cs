705e9690ceb6286a1ebcc69cf91b5320f8b62733
Christopher Dorst <chris@dorst.email>
2017-12-19 00:03:37 -0600

    Adding PackageReferenceHelper.Combine method

--- DevOps.Primitives.CSharp.Helpers.Common/PackageReferenceHelper.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/PackageReferenceHelper.cs
@@ -1,8 +1,25 @@
-﻿namespace DevOps.Primitives.CSharp.Helpers.Common
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
 {
     public static class PackageReferenceHelper
     {
+        public static IEnumerable<PackageReference> Combine(params IEnumerable<PackageReference>[] lists)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (var item in GetItems(lists))
+            {
+                if (dict.ContainsKey(item.Name)) continue;
+                dict.Add(item.Name, item.Version);
+            }
+            foreach (var item in dict) yield return new PackageReference(item.Key, item.Value);
+        }
+
         public static PackageReference Package(string name, string version)
             => new PackageReference(name, version);
+
+        private static IOrderedEnumerable<PackageReference> GetItems(IEnumerable<PackageReference>[] lists)
+            => lists.SelectMany(p => p).OrderBy(p => p.Name).ThenByDescending(p => p.Version);
     }
 }
