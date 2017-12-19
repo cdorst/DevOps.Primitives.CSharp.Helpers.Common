bf290f51c53f68de8dd856250c9997689944e32f
Christopher Dorst <chris@dorst.email>
2017-12-18 23:31:34 -0600

    Adding PackageReference type and static helper

---
+++ DevOps.Primitives.CSharp.Helpers.Common/PackageReferenceHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class PackageReferenceHelper
+    {
+        public static PackageReference Package(string name, string version)
+            => new PackageReference(name, version);
+    }
+}
