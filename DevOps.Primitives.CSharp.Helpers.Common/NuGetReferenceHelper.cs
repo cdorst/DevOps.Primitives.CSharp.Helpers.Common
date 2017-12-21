3b0fe8600887e30611adf7566aaac7cc4d699941
Christopher Dorst <chris@dorst.email>
2017-12-21 01:43:48 -0600

    Using NuGetReference type instead of PackageReference type

---
+++ DevOps.Primitives.CSharp.Helpers.Common/NuGetReferenceHelper.cs
@@ -0,0 +1,29 @@
+﻿using DevOps.Primitives.NuGet;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class NuGetReferenceHelper
+    {
+        public static IEnumerable<NuGetReference> Combine(params IEnumerable<NuGetReference>[] referenceLists)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (var reference in GetReferences(referenceLists))
+            {
+                if (dict.ContainsKey(reference.Include)) continue;
+                dict.Add(reference.Include, reference.Version);
+            }
+            foreach (var item in dict) yield return new NuGetReference(item.Key, item.Value);
+        }
+
+        public static NuGetReference Package(string name, string version)
+            => new NuGetReference(name, version);
+
+        private static IOrderedEnumerable<NuGetReference> GetReferences(IEnumerable<NuGetReference>[] referenceLists)
+            => referenceLists
+                .SelectMany(references => references)
+                .OrderBy(reference => reference.Include)
+                .ThenByDescending(reference => reference.Version);
+    }
+}
