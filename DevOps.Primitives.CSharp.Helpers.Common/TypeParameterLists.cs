605debb6571deb0bbe2fb2f4a3415fc0594a3e10
Christopher Dorst <chris@dorst.email>
2018-03-09 20:50:32 -0600

    Add TypeParameterLists helper

---
+++ DevOps.Primitives.CSharp.Helpers.Common/TypeParameterLists.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class TypeParameterLists
+    {
+        public static TypeParameterList Create(params TypeParameter[] parameters)
+            => new TypeParameterList(GetListItems(parameters));
+
+        public static TypeParameterList Create(params string[] parameters)
+            => Create(parameters.Select(p => new TypeParameter(p)).ToArray());
+
+        private static List<TypeParameterListAssociation> GetListItems(params TypeParameter[] parameters)
+            => parameters.Select(parameter => new TypeParameterListAssociation(parameter)).ToList();
+    }
+}
