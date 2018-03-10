6c4884bfb40a2dfc0552bceab589363955e274b0
Christopher Dorst <chris@dorst.email>
2018-03-09 20:57:02 -0600

    Add TypeArgumentLists helper

---
+++ DevOps.Primitives.CSharp.Helpers.Common/TypeArgumentLists.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class TypeArgumentLists
+    {
+        public static TypeArgumentList Create(params TypeArgument[] arguments)
+            => new TypeArgumentList(GetListItems(arguments));
+
+        public static TypeArgumentList Create(params string[] arguments)
+            => Create(arguments.Select(a => new TypeArgument(a)).ToArray());
+
+        private static List<TypeArgumentListAssociation> GetListItems(params TypeArgument[] arguments)
+            => arguments.Select(argument => new TypeArgumentListAssociation(argument)).ToList();
+    }
+}
