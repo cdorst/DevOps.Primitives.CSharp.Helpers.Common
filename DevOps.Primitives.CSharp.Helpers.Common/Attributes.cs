c5b8eabc964c8d42fb8cbea80de4a95322205816
Christopher Dorst <chris@dorst.email>
2018-03-11 13:29:26 -0500

    Update attribute argument syntax

--- DevOps.Primitives.CSharp.Helpers.Common/Attributes.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/Attributes.cs
@@ -4,13 +4,13 @@
     {
         public static Attribute Key() => new Attribute(nameof(Key));
         public static Attribute ProtoContract() => new Attribute(nameof(ProtoContract));
-        public static Attribute ProtoMember(int tag) => new Attribute(nameof(ProtoMember), tag.ToString());
+        public static Attribute ProtoMember(int tag) => new Attribute(nameof(ProtoMember), $"({tag.ToString()})");
         public static Attribute Required() => new Attribute(nameof(Required));
         public static Attribute Table(string name, string schema = null)
         {
             var args = $"\"{name}\"";
             if (!string.IsNullOrWhiteSpace(schema)) args = $"{args}, Schema = \"{schema}\"";
-            return new Attribute(nameof(Table), args);
+            return new Attribute(nameof(Table), $"({args})");
         }
     }
 }
