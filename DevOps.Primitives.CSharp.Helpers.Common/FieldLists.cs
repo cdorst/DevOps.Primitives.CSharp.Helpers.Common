4fc9071cedb08e0ac125b715ea3b5a4f028ebd98
Christopher Dorst <chris@dorst.email>
2018-03-14 23:11:18 -0500

    Add FieldLists helper and const Field helpers

---
+++ DevOps.Primitives.CSharp.Helpers.Common/FieldLists.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.CSharp.Helpers.Common
+{
+    public static class FieldLists
+    {
+        public static FieldList Create(params Field[] fields)
+            => new FieldList(GetListItems(fields));
+
+        private static List<FieldListAssociation> GetListItems(params Field[] fields)
+            => fields.Select(field => new FieldListAssociation(field)).ToList();
+    }
+}
--- DevOps.Primitives.CSharp.Helpers.Common/Fields.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/Fields.cs
@@ -2,11 +2,27 @@
 {
     public static class Fields
     {
+        public static Field PrivateConst(string name, string type, string summaryComment = null, AttributeListCollection attributes = null)
+            => new Field(
+                name,
+                type,
+                ModifierLists.PrivateConst,
+                Comments.Summary(summaryComment),
+                attributeListCollection: attributes);
+
         public static Field PrivateReadonly(string name, string type, string summaryComment = null, AttributeListCollection attributes = null)
             => new Field(
                 name,
                 type,
                 ModifierLists.PrivateReadonly,
+                Comments.Summary(summaryComment),
+                attributeListCollection: attributes);
+
+        public static Field PublicConst(string name, string type, string summaryComment = null, AttributeListCollection attributes = null)
+            => new Field(
+                name,
+                type,
+                ModifierLists.PublicConst,
                 Comments.Summary(summaryComment),
                 attributeListCollection: attributes);
     }
