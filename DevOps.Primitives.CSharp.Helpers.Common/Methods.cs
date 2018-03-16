1befc43cf099974e54c8371aa1f233db4634b063
Christopher Dorst <chris@dorst.email>
2018-03-16 04:59:25 -0500

    Add PublicAsync and PublicStaticAsync Methods overloads

--- DevOps.Primitives.CSharp.Helpers.Common/Methods.cs
+++ DevOps.Primitives.CSharp.Helpers.Common/Methods.cs
@@ -11,10 +11,22 @@
         public static Method Public(string identifier, string type, Block body, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
             => new Method(identifier, type, parameterList, body, ModifierLists.Public, documentationCommentList, attributes);
 
+        public static Method PublicAsync(string identifier, string type, string arrowClauseExpression, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
+            => new Method(identifier, type, arrowClauseExpression?.RemoveTrailingSemicolon(), parameterList, ModifierLists.PublicAsync, documentationCommentList, attributes);
+
+        public static Method PublicAsync(string identifier, string type, Block body, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
+            => new Method(identifier, type, parameterList, body, ModifierLists.PublicAsync, documentationCommentList, attributes);
+
         public static Method PublicStatic(string identifier, string type, string arrowClauseExpression, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
             => new Method(identifier, type, arrowClauseExpression?.RemoveTrailingSemicolon(), parameterList, ModifierLists.PublicStatic, documentationCommentList, attributes);
 
         public static Method PublicStatic(string identifier, string type, Block body, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
             => new Method(identifier, type, parameterList, body, ModifierLists.PublicStatic, documentationCommentList, attributes);
+
+        public static Method PublicStaticAsync(string identifier, string type, string arrowClauseExpression, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
+            => new Method(identifier, type, arrowClauseExpression?.RemoveTrailingSemicolon(), parameterList, ModifierLists.PublicStaticAsync, documentationCommentList, attributes);
+
+        public static Method PublicStaticAsync(string identifier, string type, Block body, ParameterList parameterList = null, DocumentationCommentList documentationCommentList = null, AttributeListCollection attributes = null)
+            => new Method(identifier, type, parameterList, body, ModifierLists.PublicStaticAsync, documentationCommentList, attributes);
     }
 }
